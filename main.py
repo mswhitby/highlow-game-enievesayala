e51a189ec750acce5a54c180045f7c8c728219f4
enievesayala <enievesayala@gmail.com>
2025-03-21 16:32:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World");
+import random
+
+
+
+
+x = int(input("Type a number from 1-100:"))
+
+random_num = random.randint(1, 100)
+
+while x != random_num:
+    if random_num > x:
+        x = int(input("Higher."))
+    else:
+        if random_num < x:
+            x = int(input("Lower."))
+else:
+    if random_num == x:
+        print("Congratulations! You've won!");
