57ed828eb6f814d5a5e959c2849fa877fbea5683
enievesayala <enievesayala@gmail.com>
2025-03-25 16:02:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ x = int(input("Type a number from 1-100:"))
 
 random_num = random.randint(1, 100)
 
-while x != random_num:
+for i in range(5):
     if random_num > x:
         x = int(input("Higher."))
     else:
@@ -16,3 +16,6 @@ while x != random_num:
 else:
     if random_num == x:
         print("Congratulations! You've won!");
+
+if i != 5:
+    print("Sorry, you've lost.");
